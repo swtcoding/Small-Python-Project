f1e4523e170a41fd89a45a328b8730c898f75410
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-01-17 00:02:18 +0800

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+
+noun1 = input("Noun: ")
+adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+noun2 = input("Noun: ")
+adj2 = input("Adjective: ")
+noun3 = input("Noun: ")
+
+
+
+madlibs = f"Humans need a reasonably accurate view of {noun1} in order to survive. If your model of reality is wildly different from the {adj1} world, then you struggle to {verb1} effective actions each day. However, truth and accuracy are not the only things that matter to {noun2}. Humans also seem to have a {adj2} {noun3} to belong."
+
+print(madlibs)

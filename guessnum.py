5f6639cac313d04b17ee0d623eff494168403f0b
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-01-18 00:08:10 +0800

    Create guessnum.py

---
+++ guessnum.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_num = random.randint(1, x)
+    guess = 0
+    while guess != random_num:
+        guess = int(input(f"Guess a number between 1 and {x}:"))
+        if guess < random_num:
+            print("It is too low.")
+        elif guess > random_num:
+            print("It is too high.")
+    
+    print("Congratulations! You have nailed it. The number is exactly {random_num}!")
+
+guess(100)

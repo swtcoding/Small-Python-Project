969f42e71bffe91cb8bc48ff967a973538c0b2e2
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-01-20 18:40:14 +0800

    Update rockpaper.py

--- rockpaper.py
+++ rockpaper.py
@@ -1,20 +1,20 @@
 import random
 
 def play():
-    user = input("What will you choose? 'r' for rock, 'p' for paper, 's' for scissor ")
+    user = input("What will you choose? 'r' for rock, 'p' for paper, 's' for scissor\n")
     computer = random.choice(['r', 'p', 's'])
     
     if user == computer:
         return "It\'s a tie!"
 
-    if win():
+    if win(user, computer):
         return "You won!"
 
-def win()
+    return "You lost!"
 
-    if (user == "r" and computer == "s") or (user =="s" and computer == "p") or (user == "s" and computer == "p"):
+def win(user, computer):
+
+    if (user == "r" and computer == "s") or (user =="s" and computer == "p") or (user == "p" and computer == "r"):
         return True
 
-return "You lost!"
-
-print(play)
+print(play())

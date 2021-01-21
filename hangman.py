c41a8eb21d71870312d567fbb88831354987c583
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-01-21 21:21:52 +0800

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,13 @@
+import random
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word:
+        word = random.choice(words)
+    
+    return word.upper()
+
+def hangman():
+
+

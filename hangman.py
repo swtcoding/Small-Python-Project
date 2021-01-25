f79d832cf9a2ae519559ecbdfcf0d6bd2f852395
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-01-25 23:51:23 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -11,9 +11,14 @@ def get_valid_word(words):
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
     
     while len(word) > 0:
-        user_word = input("Guess a word:")
-        if user
+        user_letters = input("Guess a word:")
+        if user_letters in alphabet - used_letters:
+            used_letters.append(user_letters)
+            if user_letters in word_letters:
+                word_letters.remove(user_letters)
 
 

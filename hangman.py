7b5cbeaabd67ee8eab03c6505989789b6f9aec71
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-02-04 01:01:37 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -18,11 +18,11 @@ def hangman():
     while len(word) > 0:
 
         print("You have gussed: " + " ".join(used_letters))
-        user_letters = input("Guess a word:")
 
         word_list = [letter if letter in used_letters else '_' for letter in word]
         print("Current words ", " ".join(word_list))
 
+        user_letters = input("Guess a word:")
         if user_letters in alphabet - used_letters:
             used_letters.add(user_letters)
             if user_letters in word_letters:
@@ -31,5 +31,7 @@ def hangman():
             print("The character has been used. Try another one!")
         else:
             print("Invalid choice. Please try again.")
+
+    print("Bingo! You have guessed the word!")
 
 print(hangman())

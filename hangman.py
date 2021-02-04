78447619960716a3ae84b0f968b0b55e5159bef3
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-02-04 15:22:07 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -22,7 +22,7 @@ def hangman():
         word_list = [letter if letter in used_letters else '_' for letter in word]
         print("Current words ", " ".join(word_list))
 
-        user_letters = input("Guess a word:")
+        user_letters = input("Guess a word: ")
         if user_letters in alphabet - used_letters:
             used_letters.add(user_letters)
             if user_letters in word_letters:

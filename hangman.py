d242fe957e22a129302f709bef8fbecbbfc7de78
swtcoding <73886787+swtcoding@users.noreply.github.com>
2021-02-04 15:48:19 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 def get_valid_word(words):
     word = random.choice(words)
@@ -15,13 +16,15 @@ def hangman():
     used_letters = set()
     
     while len(word) > 0:
+        print("You have gussed: ", " ".join(used_letters))
 
-        print("You have gussed: " + " ".join(used_letters))
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print("Current words: ", " ".join(word_list))
 
         user_letters = input("Guess a word:")
 
         if user_letters in alphabet - used_letters:
-            used_letters.append(user_letters)
+            used_letters.add(user_letters)
             if user_letters in word_letters:
                 word_letters.remove(user_letters)
         elif user_letters in used_letters:
@@ -29,4 +32,4 @@ def hangman():
         else:
             print("Invalid choice. Please try again.")
 
-print(hangman())
+hangman()
